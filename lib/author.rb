6d9ea63894c87927344f854a06fe431713e22b73
Janice Pena <jipena94@gmail.com>
2019-05-21 04:50:55 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,31 +1,26 @@
 class Author
-  attr_accessor :name, :songs, :song_count
+  attr_accessor :name , :posts
   
-  @@song_count = 0
  
   def initialize(name)
     @name = name
-    @songs = []
-    @@song_count += 1
+    @posts = []
   end
  
-  def add_song(song)
-    @songs << song
-    song.artist = self
+  def add_post(post)
+    @posts << post
+    post.author = self
   end
  
-  def songs
-    @songs
+  def posts
+    @post
   end
   
-  def add_song_by_name(name)
-    song = Song.new(name)
-    @songs << song
-    song.artist = self
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    post.author = self
   end
-  
-  def self.song_count
-    @@song_count
-  end
+
   
 end
