f69638a3183c0db7cd2d22ba55b0728f7e49bae6
Janice Pena <jipena94@gmail.com>
2019-05-21 04:57:42 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,7 @@
 class Author
   attr_accessor :name , :posts, :post_count
   
-  @post_count = 0 
+  @@post_count = 0 
   
   def initialize(name)
     @name = name
@@ -11,7 +11,7 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
-    @post_count += 1
+    @@post_count += 1
   end
  
   def posts
@@ -22,7 +22,9 @@ class Author
     post = Post.new(title)
     @posts << post
     post.author = self
+    @@post_count += 1
   end
-
+  
+  def 
   
 end
