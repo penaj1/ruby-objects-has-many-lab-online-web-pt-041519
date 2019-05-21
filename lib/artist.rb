a4e5370f45fc8cb143a456676325bc039ee563f8
Janice Pena <jipena94@gmail.com>
2019-05-21 04:56:11 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,7 +6,6 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    @@song_count += 1
   end
  
   def add_song(song)
@@ -23,7 +22,7 @@ class Artist
     song = Song.new(name)
     @songs << song
     song.artist = self
-     @@song_count += 1
+    @@song_count += 1
   end
   
   def self.song_count
